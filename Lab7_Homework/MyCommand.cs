f574878e888d40b206b79d183d7ca93e008c2ede
Rassokhin Anton <antonrassohin@gmail.com>
2021-12-29 22:08:59 +0300

    Добавьте файлы проекта.

---
+++ Lab7_Homework/MyCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab7_Homework
+{
+    class MyCommand
+    {
+        public static RoutedCommand Help { get; set; }
+        static MyCommand()
+        {
+            Help = new RoutedCommand();
+        }
+    }
+}
